531ace16f5089a590b84b0ae8caa4cf6b376b440
Autur Nasonov <Yasha3651@mail.ru>
2022-02-14 18:22:31 +0900

    satting page working

--- app/Http/Livewire/Admin/Setting.php
+++ app/Http/Livewire/Admin/Setting.php
@@ -4,34 +4,46 @@ namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
 use App\Models\Option;
+use Illuminate\Http\Request;
 
 class Setting extends Component
 {
     public $register_point;
-    
-    public function UpdatedRegisterPoint($value)
+    public $setting;
+    public $site_options;
+
+    public function updatedRegisterPoint($value)
     {
         $this->register_point = $value;
-        $this->setting->register_point = $value;
     }
+
+    public function mount(Request $request)
+    {
+        $this->setting = Option::get();
+        $this->site_options = [];
+        foreach($this->setting as $row)
+        {
+            $this->site_options[$row->key] = $row->val;
+        }
+
+        $this->register_point = $this->site_options['register_point'];
+    }
+
     public function save()
     {
-        dd($this->setting);
+        //dd($this->setting);
+        Option::updateOrCreate(['key'=>'register_point'],
+                                ['val'=>$this->register_point]
+                            );
     }
 
     public function render()
     {
-        $this->setting = Option::get();
-        $site_options = [];
-        foreach($this->setting as $row)
-        {
-            $site_options[$row->key] = $row->val;
-        }
         return view('livewire.admin.setting')
                 ->extends('admin.admin_master'
                      , compact( [] ) 
                     )
             ->section('dashboard_content')
-            ->with('setting'               , $site_options );
+            ->with('setting'               , $this->site_options );
     }
 }
